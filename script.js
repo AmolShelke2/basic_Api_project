afcd172aab18c1b46875c6e05e02df43360b0811
Amol shelke <shelkeamol897@gmail.com>
2022-07-05 20:32:58 +0530

    Feat: fetched cat images

---
+++ script.js
@@ -0,0 +1,12 @@
+// Getting cat
+
+const catImg = document.querySelector(".cat-img");
+const getCatbtn = document.querySelector(".get-cat");
+
+const catAPiURl = "https://api.thecatapi.com/v1/images/search";
+
+getCatbtn.addEventListener("click", async function () {
+  const res = await fetch(catAPiURl);
+  const data = await res.json();
+  catImg.src = data[0].url;
+});
