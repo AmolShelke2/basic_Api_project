66a9152f755d9819ac897c9bfe51a9615cae9fa5
Amol shelke <shelkeamol897@gmail.com>
2022-08-19 11:31:28 +0530

    Fetched Joke From Dad Jokes Api

--- Joke App/script.js
+++ Joke App/script.js
@@ -1,1 +1,16 @@
-const API_URL = "https://icanhazdadjoke.com/";
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  const res = await fetch("https://icanhazdadjoke.com", config);
+  const data = await res.json();
+  jokeEl.innerHTML = data.joke;
+}
+
+generateJoke();
