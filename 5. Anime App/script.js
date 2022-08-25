01a2beb33732c67e3a35d9dcc406b67c93fef159
Amol shelke <shelkeamol897@gmail.com>
2022-08-25 17:53:06 +0530

    Updated Script.js and added cover property to anime image

--- 5. Anime App/script.js
+++ 5. Anime App/script.js
@@ -1,1 +1,14 @@
-// Working on Javascript.
+const getBtn = document.getElementById("btn");
+const animeBox = document.querySelector(".anime-box");
+const anime = document.querySelector(".anime");
+const animeName = document.querySelector(".hero-name");
+
+const api_url = `https://api.catboys.com/img`;
+
+getBtn.addEventListener("click", async function () {
+  const response = await fetch(api_url);
+  const data = await response.json();
+  anime.src = data.url;
+  animeBox.style.display = "block";
+  animeName.textContent = data.artist;
+});
