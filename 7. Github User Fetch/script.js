f266b3328a9aead160d90c18bbb594326507f679
Amol shelke <shelkeamol897@gmail.com>
2022-08-26 21:47:02 +0530

    Fetching users using Github API

---
+++ 7. Github User Fetch/script.js
@@ -0,0 +1,7 @@
+const APIURL = "https://api.github.com/users/";
+
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const main = document.getElementById("main");
+
+async function getUser(userName) {}
