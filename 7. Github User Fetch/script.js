3bd4526a3c37437bec90d968533b6eb9d743fccb
Amol shelke <shelkeamol897@gmail.com>
2022-08-27 13:16:40 +0530

    Fetched github user and created Card component

--- 7. Github User Fetch/script.js
+++ 7. Github User Fetch/script.js
@@ -4,4 +4,38 @@ const form = document.getElementById("form");
 const search = document.getElementById("search");
 const main = document.getElementById("main");
 
-async function getUser(userName) {}
+async function getUser(userName) {
+  try {
+    const { data } = await axios(APIURL + userName);
+    console.log(data);
+    createUserCard(data);
+  } catch (err) {
+    if (err.response.status === 404) {
+      console.log(err);
+    }
+  }
+}
+
+function createUserCard(user) {
+  const cardHtml = `
+        <div class='card'>
+         <div>
+            <img src="${user.avatar_url}" alt="${user.name}" class="avatar"/>
+         </div>
+         <div class='user-info'>
+            <h2>${user.name}</h2>
+            <p>${user.bio}</p>
+
+            <ul>
+                <li>${user.followers}<strong>Followers</strong></li>
+                <li>${user.following}<strong>Following</strong></li>
+                <li>${user.public_repos}<strong>Repos</strong></li>
+            </ul>
+         </div>
+        </div>
+    `;
+
+  main.innerHTML = cardHtml;
+}
+
+getUser("AmolShelke2");
