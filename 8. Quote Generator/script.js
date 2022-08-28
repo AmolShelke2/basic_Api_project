206a4b76e19644228bd7a7825814dfd1581d7296
Amol shelke <shelkeamol897@gmail.com>
2022-08-28 22:22:38 +0530

    Quote Generator completed

--- 8. Quote Generator/script.js
+++ 8. Quote Generator/script.js
@@ -1,9 +1,15 @@
 const apiUrl = "https://api.quotable.io/random";
 
+const QoutePlaceholder = document.getElementById("quote");
+const GetQuateBtn = document.getElementById("btn");
+const author = document.querySelector(".author");
+
+GetQuateBtn.addEventListener("click", getQuote);
+
 async function getQuote() {
   const res = await fetch(apiUrl);
   const data = await res.json();
   console.log(data);
+  QoutePlaceholder.innerHTML = data.content;
+  author.textContent = `"${data.author}`;
 }
-
-getQuote();
