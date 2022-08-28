072a00c8e5c2307027cbcc1496c89c5028f3b93b
Amol shelke <shelkeamol897@gmail.com>
2022-08-28 22:08:50 +0530

    Fetched Quote adding it to the UI

--- 8. Quote Generator/script.js
+++ 8. Quote Generator/script.js
@@ -1,1 +1,9 @@
-// Fetching Quotes
+const apiUrl = "https://api.quotable.io/random";
+
+async function getQuote() {
+  const res = await fetch(apiUrl);
+  const data = await res.json();
+  console.log(data);
+}
+
+getQuote();
